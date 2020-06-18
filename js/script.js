0de5c86ddd0ae69df73a780513386ceb28d13e84
Luckyman <luckymanlukman19@gmail.com>
2020-06-18 11:31:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//SideBar
+    const sideNav = document.querySelector('.sidenav');
+    M.Sidenav.init(sideNav, {});
+
+    //Slider
+    const slider = document.querySelector('.slider');
+    M.Slider.init(slider, {
+        indicators: false,
+        height: 500,
+        transition: 500,
+        interval: 6000
+    });
+
+    // Autocomplete
+    const ac = document.querySelector('.autocomplete');
+    M.Autocomplete.init(ac, {
+        data: {
+            "Aruba": null,
+            "Cianjur": null,
+            "Cipanas": null,
+            "Bogor": null,
+            "Tegallega": null,
+            "Ciamis": null,
+            "Cipanas": null,
+        }
+    });
+
+    //Material Boxed Gallery
+    const mb = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(mb, {});
+    //Scollspy
+    const ss = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(ss, {});
